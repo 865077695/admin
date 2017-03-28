eb695bb59b68a2b55d2f203005056607c8f18e9f
zzz <865077695@qq.com>
2017-03-28 10:59:25 +0800

    修改依赖模块加载时机

--- dev/script/index.js
+++ dev/script/index.js
@@ -7,15 +7,12 @@ define(["require", "layer"], function (require,layer)
     layer.config({
         path: './res/lib/layer/'              //layer.js的文件路径,注意加斜杠
     });
-    
+    //加载动画开始执行
+    var loadDom = layer.load(1, {
+        shade: [0.1, '#fff'], //0.1透明度的白色背景
+        area: ['37px', '37px']
+    });
     require(["angular","routes","myDirective"],function(angular){
-        
-        var loadDom = layer.load(1, {
-            shade: [0.1, '#fff'], //0.1透明度的白色背景
-            area: ['37px', '37px']
-        });
-    
-    
         require(['domReady!'], function (document)
         {     //domReady依赖的!前缀来强制require()回调函数在执行之前等待DOM Ready。
         
@@ -24,7 +21,6 @@ define(["require", "layer"], function (require,layer)
             {
                 angular.bootstrap(document,['app']);
                 layer.close(loadDom);
-            
             });
         });
     })
