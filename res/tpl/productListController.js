c8db0b6f40be294a5ecd7566b2ff1e19e3223890
zzz <865077695@qq.com>
2017-03-29 15:03:16 +0800

    产品列表部分

---
+++ res/tpl/productListController.js
@@ -0,0 +1,47 @@
+/**
+ * Created by zzq on 2017/3/29.
+ */
+define(function(require){
+    var app = require('app');
+    var loadCss = require('loadCss');
+    loadCss.loadCss('res/lib/ngDialog/css/ngDialog.min.css');
+    require('myService');
+    app.controller('productListController',function($scope,myService){
+        $scope.model = function(){
+            var addProduct = layer.open({
+                type:2,
+                content:'res/tpl/addProduct.html',
+                area:['800px','500px'],
+                btn:['提交','返回'],
+                yes:function(){
+                    console.log('yes');
+                    layer.close(addProduct);
+                },
+                btn2:function(){
+                    console.log('no');
+                }
+            })
+        };
+        myService.getJson('res/json/productList1.json')
+            .success(function(data){
+                $scope.page = data.page;
+                $scope.productList = data.productList;
+                $scope.nextPage = function(){
+                    $scope.url = 'res/json/productList'+(++$scope.page)+'.json';
+                    myService.getJson($scope.url)
+                        .success(function(data){
+                            $scope.page = data.page;
+                            $scope.productList = data.productList;
+                        })
+                };
+                $scope.prevPage = function(){
+                    $scope.url = 'res/json/productList'+(--$scope.page)+'.json';
+                    myService.getJson($scope.url)
+                        .success(function(data){
+                            $scope.page = data.page;
+                            $scope.productList = data.productList;
+                        })
+                }
+            })
+    })
+})
